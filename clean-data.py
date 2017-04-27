a256e872adaea1fb0450ec8ab84b98b065c39785
nbalkiss <nbalkiss@redhat.com>
2017-04-27 11:45:15 -0400

    initial commit

---
+++ clean-data.py
@@ -0,0 +1,85 @@
+import csv
+from datetime import datetime
+from dateutil.parser import parse
+
+
+class DataCleaner():
+	"""Parses entity information from CSV, sanitizes, and writes to CSV"""
+
+	statesDict = {}
+
+	def __init__(self, statesFileName='state_abbreviations.csv'):
+		self.__loadStatesFile(statesFileName)
+
+	def __loadStatesFile(self, fileName):
+		with open(fileName) as f:
+		    reader = csv.reader(f)
+		    for row in reader:
+				self.statesDict[row[0]] = row[1]
+
+
+	def readCsvAndWriteSolutionCsv(self, inputFileName, outputFileName):
+		data = self.__readCsv(inputFileName)
+		self.__writeCsv(data)
+
+	def __readCsv(self, fileName):
+		data = []
+		with open(fileName) as f:
+		    reader = csv.DictReader(f)
+		    for row in reader:
+		    	data.append(self.__sanitizeEntity(row))
+		return data
+
+	# data should be in format of list of dicts (i.e. [{},{}])
+	def __writeCsv(self, data):
+		keys = data[0].keys()
+		with open('solution.csv', 'wb') as output_file:
+		    dict_writer = csv.DictWriter(output_file, keys)
+		    dict_writer.writeheader()
+		    dict_writer.writerows(data)
+
+
+	def __sanitizeEntity(self, entity):
+
+		# clean bio string
+		dirtyBio = entity['bio']
+		entity['bio'] = self.__cleanBio(dirtyBio)
+
+		# replace state abrev. with full state name
+		stateAbr = entity['state']
+		entity['state'] = self.__convertToFullStateName(stateAbr)
+
+		# normalize date format to YYYY-MM-DD
+		dateString = entity['start_date']
+		normalizedDate = self.__normalizeDateFormats(dateString)
+
+		# if invalid date format, add extra field to entity with 'start_date_description'
+		if(not normalizedDate):
+			entity['start_date_description'] = entity['start_date']
+			entity['start_date'] = None
+		else:
+			entity['start_date'] = normalizedDate
+			entity['start_date_description'] = None
+
+		return entity
+
+
+	def __cleanBio(self, bioString):
+		# removes extra spaces and newline characters
+		return " ".join(bioString.split())
+
+	def __convertToFullStateName(self, stateAbr):
+		return self.statesDict[stateAbr]
+
+	def __normalizeDateFormats(self, dateString):
+		# try to parse date format or return None if invalid
+		try:
+			dt = parse(dateString)
+			return dt.strftime('%Y-%m-%d')
+		except ValueError:
+			return None
+
+
+
+dataCleaner = DataCleaner()
+dataCleaner.readCsvAndWriteSolutionCsv('test.csv','solution.csv')
---
+++ scrape-web.py
@@ -0,0 +1,119 @@
+from bs4 import BeautifulSoup
+from urlparse import urljoin
+import urllib2, re, json
+
+
+class WebScraper():
+
+    """Scrape company data"""
+    rootUrl = None
+    urlsVisited = {}
+    urlQueue = []
+    failed = []
+
+    def __init__(self, url="http://data-interview.enigmalabs.org/companies/"):
+        self.rootUrl = url
+
+    def scrape(self):
+        urlsVisited = {}
+        failed = []
+        allData = []
+
+        # add initial set of urls to get started
+        self.__parseAndAddUrls(self.rootUrl)
+
+        while self.urlQueue:
+
+            # pop new url from queue
+            urlToVisit = self.urlQueue.pop(0)
+
+            print "visiting url: " + urlToVisit
+
+            # discover new, univisited urls
+            self.__parseAndAddUrls(urlToVisit)
+
+            # if this is a company page, parse company data
+            companyPageRegex = re.compile("companies/([A-Z]|[a-z])")
+            if(companyPageRegex.search(urlToVisit)):
+                companyData = self.__parseCompanyInformation(urlToVisit)
+
+                # if succesfully parsed
+                if companyData:
+                    allData.append(companyData)
+
+        # print json object
+        self.__writeJson(allData,"solution.json")
+
+        # print failed urls
+        self.__writeJson(self.failed,"failed.json")
+
+
+    def __parseAndAddUrls(self, url):
+
+        print "dicovering new urls"
+
+        # get soup for url
+        soup = self.__getSoup(url)
+
+        # failed to parse
+        if not soup:
+            return
+
+        # get all links to other pages
+        for tag in soup.find_all("a"):
+
+            # join base to relative url
+            urlToVisit = self.__joinRelativeUrl(tag['href'])
+
+            # addToQueue
+            if(urlToVisit not in self.urlsVisited):
+
+                # mark that we have visited this url
+                self.urlsVisited[urlToVisit] = 1
+
+                # add to queue for scraping
+                self.urlQueue.append(urlToVisit)
+
+
+    def __parseCompanyInformation(self, url):
+
+        print "parsing company data"
+
+        companySoup = self.__getSoup(url)
+
+        # if failed
+        if not companySoup:
+            return None
+
+        company = {}
+
+        # parse table into dictionary
+        for row in companySoup.findAll('tr'):
+            aux = row.findAll('td')
+            company[aux[0].string] = aux[1].string
+
+        return company
+
+
+    def __joinRelativeUrl(self, relativeUrl):
+        return "%20".join(urljoin(self.rootUrl,relativeUrl).split(" "))
+
+
+    def __getSoup(self, url):
+        try:
+            doc = urllib2.urlopen(url, timeout=10).read()
+            soup = BeautifulSoup(doc, 'html.parser')
+            return soup
+        except:
+            self.failed.append(url)
+            print "failed on: " + url
+            return None
+
+    def __writeJson(self, data, fileName):
+        with open(fileName, 'w') as outfile:
+            json.dump(data, outfile)
+
+
+
+webScraper = WebScraper("http://data-interview.enigmalabs.org/companies/")
+webScraper.scrape()
